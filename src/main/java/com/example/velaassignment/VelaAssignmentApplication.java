9e3aabbc1cd10f7336ebaaf06b1cc376382bd2b8
thanhdev <thanhpdhe141032@gmail.com>
2024-06-07 14:20:08 +0700

    innitial commit

---
+++ src/main/java/com/example/velaassignment/VelaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.velaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelaAssignmentApplication.class, args);
+    }
+
+}
